13a155643be14a1b17565f9dec51d82c87bb24ec
marfushhaaa <p1m455w96@gmail.com>
2026-09-21 14:17:01 +0200

    feat: add a helper function for easy access of current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :current_user
 
+  def current_user
+    # If session[:user_id] is nil, set it to nil, otherwise find the user by id.
+    @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+  end
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 end

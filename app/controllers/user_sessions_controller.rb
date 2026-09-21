400e62bed58be592113e1474682fa4377988a942
marfushhaaa <p1m455w96@gmail.com>
2026-09-21 14:03:04 +0200

    feat: add logik in user_session_controller

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,17 @@
 class UserSessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      flash[:alert] = "Login failed"
+      redirect_to new_user_session_path
+    end
   end
 end

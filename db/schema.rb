231702a7d2a6a499e97be49f7674e21850c1f26a
marfushhaaa <p1m455w96@gmail.com>
2026-09-24 08:45:58 +0200

    feat: add boolean admin column in table Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_21_074932) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_24_063913) do
   create_table "bookings", force: :cascade do |t|
     t.integer "course_id", null: false
     t.datetime "created_at", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_21_074932) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "password_digest", null: false

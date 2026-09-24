19382d454cefce07fe825eb69b3d0250844c66ce
marfushhaaa <p1m455w96@gmail.com>
2026-09-24 09:49:34 +0200

    feat: add validation to courses.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,16 @@
 class Course < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :bookings, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :start_date, :end_date, presence: true
+  validates :max_participants, numericality: { greater_than: 0 }
+  validate :end_after_start
+
+  private
+
+  def end_after_start
+    return if start_date.blank? || end_date.blank?
+    errors.add(:end_date, "muss nach dem Start liegen") if end_date <= start_date
+  end
 end

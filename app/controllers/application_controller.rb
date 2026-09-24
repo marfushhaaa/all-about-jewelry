c598f7099157d5b48900ab92fb66cf7ee47c8d47
marfushhaaa <p1m455w96@gmail.com>
2026-09-24 08:57:44 +0200

    feat: add policies for authorisation control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   include Authentication
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def pundit_user
+    Current.user
+  end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   helper_method :current_user
